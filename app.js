d847fcb680e12d1db0bd131f298e976785a8b163
Anna <nosenkova2013@inbox.ru>
2021-04-09 19:19:31 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 $('.menu_btn').on('click', function(e){
-   e.preventDefault;
-   $(this).toggleClass('menu_btn_active');
-   $('.slide_menu').toggleClass('slide_menu_active');
-   $('.nav_menu').toggleClass('nav_menu_active');
-   });
+	e.preventDefault;
+	$(this).toggleClass('menu_btn_active');
+	$('.slide_menu').toggleClass('slide_menu_active');
+	$('.nav_menu').toggleClass('nav_menu_active');
+	$('.poloska1').toggleClass('poloska_active');
+	$('.poloska2').toggleClass('poloska_active');
+	$('.poloska3').toggleClass('poloska_active');
+});
+$('.menu_link_btn').on('click', function(e){
+	e.preventDefault;
+	$('.planet_list').toggleClass('planet_list_active');
+	$('.arrow').toggleClass('arrow_active');
+	$('.poloska_active').toggleClass('poloska_act');
+	$('.poloska_active').toggleClass('poloska_act');
+	$('.poloska_active').toggleClass('poloska_act');
+});
+
 
    function countdown(dateEnd) {
-      var timer, days, hours, minutes, seconds;
+      var timer, days, hours, minutes, seconds; // определение функции отсчета
     
-      dateEnd = new Date(dateEnd);
+      dateEnd = new Date(dateEnd); // присваивание значения 
       dateEnd = dateEnd.getTime();
     
       if (isNaN(dateEnd)) {
         return;
       }
     
-      timer = setInterval(calculate, 1000);
+      timer = setInterval(calculate, 1000); // повтор через 1 с
     
-      function calculate() {
+      function calculate() {  // вывод
         var dateStart = new Date();
         var dateStart = new Date(dateStart.getUTCFullYear(),
           dateStart.getUTCMonth(),
@@ -49,7 +61,13 @@ $('.menu_btn').on('click', function(e){
       function display(days, hours, minutes, seconds) {}
     }
     
+    var today = new Date();
+    var weekday = today.getUTCDay();
+
+    if (weekday==5) {
+      countdown (''); // отсчет на 3 дня
+    }
+
     
     
-    
-    countdown ('09/22/2021 02:00:00 AM');
+   
